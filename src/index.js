7e397cd8f4d9d565e3de89fed0d4c987fd86bd82
Ignacio Ribes <katanaweb@gmail.com>
2017-04-08 18:53:29 -0300

    delete image and add comments

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import firebase from 'firebase';
 import App from './App';
 import './index.css';
 
+// set firebase api data
 firebase.initializeApp({
     apiKey: "AIzaSyDErtHEntY89cV4BZn6htwXSPs1-hTbvEI",
     authDomain: "nachogram-55f08.firebaseapp.com",
